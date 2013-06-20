39041ada2827b42442a704cbbbf00018076b4fe9
Fábio Barreiros <fabiojjbarreiros@gmail.com>
2013-06-20 02:39:08 +0100

    Added left menu

--- Moody/src/com/example/moody/MainActivity.java
+++ Moody/src/com/example/moody/MainActivity.java
@@ -1,23 +1,54 @@
 package com.example.moody;
 
-import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.view.Menu;
-
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
-public class MainActivity extends SherlockActivity {
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
+public class MainActivity extends BaseActivity{
+	
+	private Fragment mContent;
+	
+	public MainActivity(){
+		super(R.string.app_name);
+		
+	}
+	
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		if (savedInstanceState != null)
+			mContent = getSupportFragmentManager().getFragment(savedInstanceState, "mContent");
+		if (mContent == null)
+			mContent = new MainView();
+		
+		setContentView(R.layout.content_frame);
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.content_frame, mContent).commit();
+		
+		setBehindContentView(R.layout.menu_frame);
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.menu_frame, new RandomList()).commit();
+		
+		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		setSlidingActionBarEnabled(true);
 	}
-
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getSupportMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
+	public void onSaveInstanceState(Bundle outState){
+		super.onSaveInstanceState(outState);
+		getSupportFragmentManager().putFragment(outState, "mContent", mContent);
 	}
+	
+	public void switchContent(Fragment fragment){
+		mContent = fragment;
+		getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
+		getSlidingMenu().showContent();
+	}
+	
+	
 
 }
