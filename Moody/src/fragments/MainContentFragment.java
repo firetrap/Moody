30cfae7083a28a89c61f5fb6241854d83f885665
firetrap <fabiojjbarreiros@gmail.com>
2013-09-04 14:01:28 +0100

    Started to implement onCLick for the topics content preview

--- Moody/src/fragments/MainContentFragment.java
+++ Moody/src/fragments/MainContentFragment.java
@@ -10,18 +10,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import activities.LoginActivity;
+import activities.UserDetailsActivity;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.moody.R;
 
@@ -49,7 +53,7 @@ public class MainContentFragment extends Fragment {
 	}
 
 	public View numberOfRows(JSONArray rows, String CourseName) {
-		
+
 		LayoutInflater inflater = (LayoutInflater) getActivity()
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
@@ -58,60 +62,14 @@ public class MainContentFragment extends Fragment {
 				android.view.ViewGroup.LayoutParams.MATCH_PARENT,
 				android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
 		insertPoint.setOrientation(LinearLayout.VERTICAL);
-	
+
 		View topicsHeaderView = inflater.inflate(
 				R.layout.topics_preview_header, null);
-		TextView courseName = (TextView) topicsHeaderView.findViewById(R.id.course_path_textView);
-		courseName.setText("Courses > "+ CourseName);
+		TextView courseName = (TextView) topicsHeaderView
+				.findViewById(R.id.course_path_textView);
+		courseName.setText("Courses > " + CourseName);
 		insertPoint.addView(topicsHeaderView);
 
-		
-
-//		RelativeLayout beforeTopics = new RelativeLayout(getActivity());
-//		// RelativeLayout params
-//		beforeTopics.setLayoutParams(new LayoutParams(
-//				android.view.ViewGroup.LayoutParams.MATCH_PARENT,
-//				android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
-//		beforeTopics.setBackgroundResource(R.color.C_White);
-//		beforeTopics.setId(0);
-//		beforeTopics.setTag("before_topic_tag");
-//
-//		TextView coursePath = new TextView(getActivity());
-//		coursePath.setLayoutParams(new LayoutParams(
-//				android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
-//				android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
-//		coursePath.setBackgroundColor(getResources().getColor(R.color.C_White));
-//		coursePath.setPadding(5, 10, 0, 10);
-//		coursePath.setText("Courses > " + CourseName);
-//		coursePath.setTextColor(getResources().getColor(R.color.C_Blue_Light));
-//		coursePath.setId(1);
-//		coursePath.setTag("course_path_tag");
-//		beforeTopics.addView(coursePath);
-//
-//		ImageButton addFavorites = new ImageButton(getActivity());
-//
-//		// Add favorits params
-//		addFavorites.setLayoutParams(new LayoutParams(
-//				android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
-//				android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
-//		addFavorites.setBackgroundResource(R.color.C_White);
-//		addFavorites
-//				.setContentDescription(getString(R.string.add_favorite_description));
-//		addFavorites.setPadding(0, 10, 50, 10);
-//		addFavorites.setImageResource(R.drawable.add_favorites);
-//		addFavorites.setId(2);
-//		addFavorites.setTag("add_favorites_tag");
-//
-//		RelativeLayout.LayoutParams addFavoritesParams = new RelativeLayout.LayoutParams(
-//				addFavorites.getLayoutParams());
-//
-//		addFavoritesParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-//		addFavorites.setLayoutParams(addFavoritesParams);
-//		beforeTopics.addView(addFavorites);
-
-//		insertPoint.addView(beforeTopics);
-
-		
 		for (int j = 0; j < rows.length(); j++) {
 			try {
 				JSONObject arrayCursor = rows.getJSONObject(j);
@@ -120,119 +78,13 @@ public class MainContentFragment extends Fragment {
 				row.setLayoutParams(new LayoutParams(
 						android.view.ViewGroup.LayoutParams.MATCH_PARENT,
 						android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
-				View view = inflater.inflate(
-						R.layout.topics_preview_context, null);
-				TextView topicName = (TextView) view.findViewById(R.id.topic_name);
+				View view = inflater.inflate(R.layout.topics_preview_context,
+						null);
+				TextView topicName = (TextView) view
+						.findViewById(R.id.topic_name);
 				topicName.setText(arrayCursor.getString("name"));
-				
-				
-				
 				row.addView(view);
 				insertPoint.addView(row);
-
-				//
-				// RelativeLayout topics = new RelativeLayout(getActivity());
-				// ImageButton arrow = new ImageButton(getActivity());
-				// TextView topicName = new TextView(getActivity());
-				// TextView contentPreview = new TextView(getActivity());
-				// View viewBottom = new View(getActivity());
-				// View viewTop = new View(getActivity());
-				//
-				// // RelativeLayout params
-				// topics.setLayoutParams(new LayoutParams(
-				// android.view.ViewGroup.LayoutParams.MATCH_PARENT,
-				// android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
-				// topics.setBackgroundResource(R.drawable.fill_light_grey);
-				// topics.setId(j + 3);
-				// topics.setTag("topic_tag_" + j);
-				//
-				// // Arrow params
-				// arrow.setLayoutParams(new LayoutParams(
-				// android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
-				// android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
-				// arrow.setBackgroundResource(R.color.transparent);
-				// arrow.setContentDescription(getString(R.string.arrow_description));
-				// arrow.setPadding(0, 0, 5, 0);
-				// arrow.setImageResource(R.drawable.right_arrow);
-				// arrow.setId(j + 4);
-				// arrow.setTag("arrow_tag");
-				//
-				// RelativeLayout.LayoutParams arrowParams = new
-				// RelativeLayout.LayoutParams(
-				// arrow.getLayoutParams());
-				// arrowParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-				// arrowParams.addRule(RelativeLayout.CENTER_VERTICAL);
-				// arrow.setLayoutParams(arrowParams);
-				//
-				// // Add textview topics
-				// topicName.setLayoutParams(new LayoutParams(
-				// android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
-				// android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
-				// topicName.setBackgroundResource(R.drawable.border_course_context);
-				//
-				// topicName.setPadding(20, 0, 20, 0);
-				// topicName.setText(arrayCursor.getString("name"));
-				// topicName.setId(j + 5);
-				// topicName.setTag("topic_name_tag");
-				//
-				// RelativeLayout.LayoutParams topicsNameParams = new
-				// RelativeLayout.LayoutParams(
-				// topicName.getLayoutParams());
-				// topicsNameParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-				// topicsNameParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-				// topicsNameParams.setMargins(0, 10, 0, 0);
-				// topicName.setLayoutParams(topicsNameParams);
-				//
-				// // Add textview content preview
-				// contentPreview.setLayoutParams(new LayoutParams(
-				// android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
-				// android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
-				// contentPreview.setText(getString(R.string.content_preview));
-				//
-				// RelativeLayout.LayoutParams contentPreviewParams = new
-				// RelativeLayout.LayoutParams(
-				// contentPreview.getLayoutParams());
-				// contentPreviewParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-				// contentPreviewParams.addRule(RelativeLayout.LEFT_OF,
-				// arrow.getId());
-				// contentPreviewParams.addRule(RelativeLayout.BELOW,
-				// topicName.getId());
-				// contentPreviewParams.setMargins(0, 10, 30, 10);
-				// contentPreview.setId(j + 6);
-				// contentPreview.setTag("content_preview_tag");
-				// contentPreview.setLayoutParams(contentPreviewParams);
-				//
-				// // Add view to fill space bottom of topics
-				// viewBottom.setLayoutParams(new LayoutParams(
-				// android.view.ViewGroup.LayoutParams.FILL_PARENT, 10));
-				// viewBottom.setBackgroundResource(R.drawable.border_inside);
-				// RelativeLayout.LayoutParams viewBottomParams = new
-				// RelativeLayout.LayoutParams(
-				// viewBottom.getLayoutParams());
-				// viewBottomParams.addRule(RelativeLayout.ALIGN_BOTTOM,
-				// contentPreview.getId());
-				// viewBottomParams.setMargins(0, 0, 0, -10);
-				// viewBottom.setLayoutParams(viewBottomParams);
-				//
-				// // Add view to fill space in the top of topics
-				// viewTop.setLayoutParams(new LayoutParams(
-				// android.view.ViewGroup.LayoutParams.FILL_PARENT, 10));
-				// viewTop.setBackgroundColor(getResources().getColor(R.color.C_White));
-				// RelativeLayout.LayoutParams viewTopParams = new
-				// RelativeLayout.LayoutParams(
-				// viewTop.getLayoutParams());
-				// viewTopParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-				// viewTopParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-				// viewTop.setLayoutParams(viewTopParams);
-				//
-				// topics.addView(arrow);
-				// topics.addView(topicName);
-				// topics.addView(contentPreview);
-				// topics.addView(viewBottom);
-				// topics.addView(viewTop);
-				//
-				// row.addView(topics);
-				// insertPoint.addView(row);
 
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
@@ -272,4 +124,24 @@ public class MainContentFragment extends Fragment {
 
 	}
 
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+		case R.id.add_favorites_button:
+			Toast.makeText(getActivity().getApplicationContext(),
+					"ADD FAVORITOS" + v.getId(), Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.arrow_topics_image_button:
+			Toast.makeText(getActivity().getApplicationContext(),
+					"ENTROU NO TOPICO" + v.getId(), Toast.LENGTH_SHORT).show();
+
+			break;
+
+		default:
+			// Toast.makeText(getApplicationContext(),
+			// "ENTROU NO PRIMEIRO :" + v.getId(), Toast.LENGTH_SHORT)
+			// .show();
+			// throw new RuntimeException("Unknown button ID");
+		}
+	}
 }
