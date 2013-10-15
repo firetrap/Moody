4ceb4cf16b4a80c6066ea6b0e3a7f8df1477c519
firetrap <fabiojjbarreiros@gmail.com>
2013-10-15 21:37:51 +0100

    Latest RC; Minor Issues

--- Moody/src/fragments/FragLatest.java
+++ Moody/src/fragments/FragLatest.java
@@ -2,13 +2,12 @@ package fragments;
 
 import java.util.LinkedList;
 
-import restPackage.MoodleCourseContent;
-
 import managers.ManContents;
 import managers.ManDataStore;
 import managers.ManLatest;
+import restPackage.MoodleCourseContent;
+import restPackage.MoodleModule;
 import android.app.Fragment;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,58 +19,73 @@ import android.widget.TextView;
 import com.android.moody.R;
 
 public class FragLatest extends Fragment {
+	LinkedList<ManLatest> latestList;
+	// Get from resource the number of cards per line
+	int cardsPerLine;
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		Context context = getActivity().getApplicationContext();
-		ManContents content = new ManContents(context);
+		initResources();
+		// The outer Vertical LinearLayout
+		LinearLayout outerLayout = createVerticalLinearLayout();
+		outerLayout.addView(setLayoutTitle());
+		// Calculate the number of rows to build
+		int lines = setRows(latestList, cardsPerLine);
+		for (int i = 0; i < lines; i++) {
 
-		LinkedList<ManLatest> latestList = (LinkedList<ManLatest>) new ManDataStore(
-				context).getData("Latest");
-
-		LinearLayout outerLayout = new LinearLayout(context);
-		outerLayout.setLayoutParams(new LayoutParams(
-				android.view.ViewGroup.LayoutParams.MATCH_PARENT,
-				android.view.ViewGroup.LayoutParams.MATCH_PARENT));
-		outerLayout.setOrientation(LinearLayout.VERTICAL);
-
-		outerLayout.addView(setLayoutTitle(context));
-
-		int cardsPerLine = getResources().getInteger(
-				R.integer.latest_item_per_line);
-		int colums = setRows(latestList, cardsPerLine);
-
-		for (int i = 0; i < colums; i++) {
-			ManLatest latest = latestList.get(i);
-
-			LinearLayout innerLayout = new LinearLayout(context);
-			innerLayout.setLayoutParams(new LayoutParams(
-					android.view.ViewGroup.LayoutParams.MATCH_PARENT,
-					android.view.ViewGroup.LayoutParams.MATCH_PARENT));
-			innerLayout.setOrientation(LinearLayout.HORIZONTAL);
+			// The inner Horizontal Linear Layout
+			LinearLayout innerLayout = createHorizontalLinearLayout();
 
 			for (int j = 0; j < cardsPerLine; j++) {
+				if (!latestList.isEmpty()) {
+					ManLatest latest = latestList.getFirst();
+					latestList.removeFirst();
 
-				Long topicId = Long.parseLong(latest.getTopicId());
-				String courseId = latest.getCourseId();
-				MoodleCourseContent[] courseContent = content
-						.getContent(courseId);
-				MoodleCourseContent topic = content.getTopic(topicId,
-						courseContent);
+					MoodleCourseContent[] course = new ManContents(
+							getActivity()).getContent(latest.getCourseId());
 
-				View view = inflater.inflate(R.layout.frag_latest, null);
-				view.setLayoutParams(new LayoutParams(
-						android.view.ViewGroup.LayoutParams.MATCH_PARENT,
-						android.view.ViewGroup.LayoutParams.MATCH_PARENT, 1f));
-				TextView title = (TextView) view
-						.findViewById(R.id.latest_preview_title);
-				title.setText(topic.getName());
-				TextView description = (TextView) view
-						.findViewById(R.id.latest_preview_description);
-				description.setText(topic.getSummary());
-				innerLayout.addView(view);
+					MoodleCourseContent topic = new ManContents(getActivity())
+							.getTopic(Long.parseLong(latest.getTopicId()),
+									course);
+
+					MoodleModule[] modules = topic.getMoodleModules();
+
+					View view = inflater.inflate(R.layout.frag_latest, null);
+					view.setLayoutParams(new LayoutParams(
+							android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+							android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+							1f));
+
+					TextView title = (TextView) view
+							.findViewById(R.id.latest_preview_title);
+					title.setText(topic.getName());
+
+					String moduleName = "";
+					// Loop for the modules array
+					for (int k = 0; k < topic.getMoodleModules().length; k++) {
+
+						String getNameDirty = modules[k].getName();
+						String getNamePure = "";
+
+						for (int n = 0; n < getNameDirty.split("\\s+").length; n++) {
+							if (n == 5) {
+								break;
+							}
+
+							getNamePure += getNameDirty.split("\\s+")[n] + " ";
+						}
+
+						moduleName += "-" + getNamePure + "\n";
+
+					}
+
+					TextView description = (TextView) view
+							.findViewById(R.id.latest_preview_description);
+					description.setText(moduleName);
+					innerLayout.addView(view);
+				}
+
 			}
 			outerLayout.addView(innerLayout);
 
@@ -79,6 +93,35 @@ public class FragLatest extends Fragment {
 
 		return outerLayout;
 
+	}
+
+	/**
+	 * This method is necessary because the fragment life cycle, the context
+	 * needed it's only available after the onCreate it's triggered
+	 */
+	@SuppressWarnings("unchecked")
+	private void initResources() {
+		latestList = (LinkedList<ManLatest>) new ManDataStore(getActivity())
+				.getData("Latest");
+		// Get from resource the number of cards per line
+		cardsPerLine = getResources()
+				.getInteger(R.integer.latest_item_per_line);
+	}
+
+	/**
+	 * @param context
+	 * @return
+	 */
+	private TextView setLayoutTitle() {
+		TextView large_title = new TextView(getActivity());
+		large_title.setLayoutParams(new LayoutParams(
+				android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
+				android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
+		large_title.setText("Latest Contents");
+		large_title.setTextAppearance(getActivity(), R.style.CardsLayoutTitle);
+		// large_title.setTypeface(null, Typeface.ITALIC);
+		large_title.setPadding(30, 10, 0, 10);
+		return large_title;
 	}
 
 	/**
@@ -92,25 +135,33 @@ public class FragLatest extends Fragment {
 			Double count = Math.ceil(latest.size() / 2.0);
 			colums = count.intValue();
 		} else {
-			colums = latest.size();
+			colums = 1;
 		}
 		return colums;
 	}
 
 	/**
-	 * @param context
 	 * @return
 	 */
-	private TextView setLayoutTitle(Context context) {
-		TextView large_title = new TextView(context);
-		large_title.setLayoutParams(new LayoutParams(
-				android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
-				android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
-		large_title.setText("Latest Contents");
-		large_title.setTextAppearance(context, R.style.CardsLayoutTitle);
-		// large_title.setTypeface(null, Typeface.ITALIC);
-		large_title.setPadding(30, 10, 0, 10);
-		return large_title;
+	private LinearLayout createVerticalLinearLayout() {
+		LinearLayout outerLayout = new LinearLayout(getActivity());
+		outerLayout.setLayoutParams(new LayoutParams(
+				android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+				android.view.ViewGroup.LayoutParams.MATCH_PARENT));
+		outerLayout.setOrientation(LinearLayout.VERTICAL);
+		return outerLayout;
+	}
+
+	/**
+	 * @return
+	 */
+	private LinearLayout createHorizontalLinearLayout() {
+		LinearLayout innerLayout = new LinearLayout(getActivity());
+		innerLayout.setLayoutParams(new LayoutParams(
+				android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+				android.view.ViewGroup.LayoutParams.MATCH_PARENT));
+		innerLayout.setOrientation(LinearLayout.HORIZONTAL);
+		return innerLayout;
 	}
 
 }
